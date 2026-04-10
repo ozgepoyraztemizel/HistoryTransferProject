d03223d10dfea179a8ed84265230151853dfe6c9
Özge Poyraz <ozgepoyraztemizel@gmail.com>
2026-04-10 12:11:24 +0300

    initial commit

--- HistoryProject/ViewController.swift
+++ HistoryProject/ViewController.swift
@@ -11,9 +11,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("hello world")
         // Do any additional setup after loading the view.
     }
-
+    
 
 }
 
